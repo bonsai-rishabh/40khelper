9cb2f395e3542e64ef1ddab296a292f62cd324f9
Bajpai, Rishabh 042945796 <rishabh.bajpai@nasa.gov>
2023-05-30 11:08:32 -0700

    commented out problematic lines causing build failures as they are not currently needed

--- utils/postgres-utils.ts
+++ utils/postgres-utils.ts
@@ -2,5 +2,5 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const allUnits: Units[] = await prisma.units.findMany()
+//const allUnits: Units[] = await prisma.units.findMany()
 
